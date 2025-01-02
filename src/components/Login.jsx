8a00555d420e498fe19f42579fc6d4afea19fe37
Sughosh <sughoshathreya1@gmail.com>
2025-01-02 15:50:38 +0530

    fixed the document's title

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://blogsbysughosh.xyz:8089/login", {
+      const response = await axios.post("http://localhost:8089/login", {
         username,
         password,
       });
